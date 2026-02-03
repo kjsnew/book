4711bb19672fdeb2d9c61c391719da7bb1d982ce
kjsnew <250451009+kjsnew@users.noreply.github.com>
2026-02-02 20:17:31 -0600

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.create!(
+  [
+    { title: "My Potato", author: "Charity Jones",
+    genre: "nonfiction", publish_date: "2000-09-25" },
+    { title: "Knickerbocker", author: "Nyoo Yawk",
+    genre: "fiction", publish_date: "1998-01-01" },
+    { title: "Moonie B Jones", author: "Courtney Love",
+    genre: "mystery", publish_date: "1984-02-28" },
+    { title: "Hector's Bell", author: "Hector Salamanca",
+    genre: "biography", publish_date: "09-09-2009" },
+    { title: "Final Destination", author: "Your Mind",
+    genre: "thriller", publish_date: "09-25-2001" }
+  ]
+)
